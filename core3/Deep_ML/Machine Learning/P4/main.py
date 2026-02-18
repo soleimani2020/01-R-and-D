9c1ca816ac1ab484ad08fb0e9b300415a9e680d3
Alireza Soleimani <66776733+soleimani2020@users.noreply.github.com>
2026-02-18 18:17:38 +0100

    Update main.py

--- core3/Deep_ML/Machine Learning/P4/main.py
+++ core3/Deep_ML/Machine Learning/P4/main.py
@@ -1,3 +1,5 @@
+#### Approach 1  : Replacing Matrix
+
 from typing import List, Union
 
 def scalar_multiply(matrix: List[List[Union[int, float]]], scalar: Union[int, float]) -> List[List[Union[int, float]]]:
@@ -22,6 +24,29 @@ def scalar_multiply(matrix: List[List[Union[int, float]]], scalar: Union[int, fl
     
     return matrix
 
+
+
+#### Approach 2  : Copying Matrix
+
+from typing import Union
+
+def scalar_multiply(matrix: list[list[Union[int, float]]], 
+                    scalar: Union[int, float]) -> list[list[Union[int, float]]]:
+
+    if not matrix or not matrix[0]:
+        return []  # Return empty matrix if input is empty
+    
+    p, q = len(matrix), len(matrix[0])  # Rows/Columns
+                        
+    result = [[0] * q for _ in range(p)] 
+    
+    for i in range(p):
+        for j in range(q):
+            result[i][j] = matrix[i][j] * scalar
+            
+    return result
+
+
 # Example usage
 if __name__ == "__main__":
     mat = [[1, 2, 3],
@@ -38,3 +63,4 @@ if __name__ == "__main__":
     print("\nMatrix after multiplying by", scalar, ":")
     for row in mat:
         print(row)
+
