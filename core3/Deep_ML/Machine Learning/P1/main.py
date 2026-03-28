dca35404b55bbd603bdf922cbe762befd09f1573
Alireza Soleimani <66776733+soleimani2020@users.noreply.github.com>
2026-03-28 23:09:45 +0100

    Update main.py

---
+++ core3/Deep_ML/Machine Learning/P1/main.py
@@ -0,0 +1,21 @@
+def matrix_dot_vector(a: list[list[int|float]], b: list[int|float]) -> list[int|float]:
+    # Return a list where each element is the dot product of a row of 'a' with 'b'.
+    # If the number of columns in 'a' does not match the length of 'b', return -1.
+    
+    if len(a) == 0:
+        return []
+    
+    n = len(a)      # number of rows
+    m = len(a[0])   # number of columns
+    
+    if len(b) != m:
+        return -1
+    
+    result = []
+    for row in a:
+        dot = 0
+        for matrix_element, vector_element in zip(row, b):
+            dot += matrix_element * vector_element
+        result.append(dot)
+    
+    return result
