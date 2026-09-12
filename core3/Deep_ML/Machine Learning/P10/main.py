06d744aaa2d184955159c39ba068b54b01e8e477
Alireza Soleimani <66776733+soleimani2020@users.noreply.github.com>
2026-09-12 11:06:05 +0100

    Update main.py

--- core3/Deep_ML/Machine Learning/P10/main.py
+++ core3/Deep_ML/Machine Learning/P10/main.py
@@ -1,6 +1,13 @@
 import math
-import numpy as np 
 
 def poisson_probability(k, lam):
-    val = (lam**k) * (np.exp(-lam)) / math.factorial(k)
+    """
+    Calculate the probability of observing exactly k events in a fixed interval,
+    given the mean rate of events lam, using the Poisson distribution formula.
+    :param k: Number of events (non-negative integer)
+    :param lam: The average rate (mean) of occurrences in a fixed interval
+    """
+    
+    val = math.exp(-lam) * lam**k / math.factorial(k)
+
     return round(val, 5)
