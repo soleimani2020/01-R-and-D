477210c3bd5eeedd187c1015bf7a5a07e53fcf78
Alireza Soleimani <66776733+soleimani2020@users.noreply.github.com>
2026-09-12 11:46:03 +0100

    Update main.py

--- core3/Deep_ML/Machine Learning/P14/main.py
+++ core3/Deep_ML/Machine Learning/P14/main.py
@@ -11,3 +11,37 @@ def calculate_covariance_matrix(vectors: list[list[float]]) -> list[list[float]]
     centered = vectors - means
     cov_matrix = (centered.T @ centered) / (n-1)
     return cov_matrix.tolist()
+
+
+
+def calculate_covariance_matrix(vectors: list[list[float]]) -> list[list[float]]:
+
+    n_features = len(vectors)
+    n_observations = len(vectors[0])
+
+    means = []
+
+    for feature in vectors:
+        mean = sum(feature) / n_observations
+        means.append(mean)
+
+    covariance_matrix = []
+
+    for i in range(n_features):
+        row = []
+
+        for j in range(n_features):
+            cov = 0
+
+            for k in range(n_observations):
+                cov += (
+                    (vectors[i][k] - means[i]) *
+                    (vectors[j][k] - means[j])
+                )
+
+            cov = cov / (n_observations - 1)
+            row.append(cov)
+
+        covariance_matrix.append(row)
+
+    return covariance_matrix
