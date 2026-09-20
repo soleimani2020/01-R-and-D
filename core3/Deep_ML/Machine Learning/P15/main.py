0f64d9f82d80e8a87afc8ddfb3cf847ae54b3f9f
Alireza Soleimani <66776733+soleimani2020@users.noreply.github.com>
2026-09-20 09:23:48 +0100

    Update main.py

--- core3/Deep_ML/Machine Learning/P15/main.py
+++ core3/Deep_ML/Machine Learning/P15/main.py
@@ -1,3 +1,30 @@
+##################################### Vectorized Solution 
+import numpy as np
+
+def linear_regression_gradient_descent(
+    X: np.ndarray,
+    y: np.ndarray,
+    alpha: float,
+    iterations: int
+) -> np.ndarray:
+
+    m, n = X.shape
+
+    y = y.reshape(-1, 1)
+    theta = np.zeros((n, 1))
+
+    for _ in range(iterations):
+        predicted = X @ theta
+        error = predicted - y
+
+        grad = (2 / m) * (X.T @ error)
+
+        theta = theta - alpha * grad
+
+    return theta.flatten()
+
+#####################################
+
 import numpy as np
 
 def linear_regression_gradient_descent(X: np.ndarray, y: np.ndarray, alpha: float, iterations: int) -> np.ndarray:
