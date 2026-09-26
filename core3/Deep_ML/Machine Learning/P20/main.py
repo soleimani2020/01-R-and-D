24c19a1bf8bfc0e42f567a2c74970aee2fc6c96a
Alireza Soleimani <66776733+soleimani2020@users.noreply.github.com>
2026-09-26 11:54:22 +0100

    Update main.py

--- core3/Deep_ML/Machine Learning/P20/main.py
+++ core3/Deep_ML/Machine Learning/P20/main.py
@@ -1,24 +1,28 @@
 import numpy as np
 
 def accuracy_score(y_true, y_pred):
-	total_predictions = len(y_true)
-
-
-	y_true = np.array(y_true)
-	y_pred = np.array(y_pred)
-
-	if y_true.shape != y_pred.shape:
-		raise ValueError("y_true and y_pred must have the same shape")
-
-	#correct_predictions = 0
-	#for i in range(len(y_true)):
-	#	if y_true[i] == y_pred[i]:
-	#		correct_predictions+=1
-
-	correct_predictions =np.sum(y_true==y_pred)
-
-	return correct_predictions/total_predictions
-
-
-
+	# Your code here
+	correct = np.sum(y_true == y_pred)
+	Total = len(y_true)
 	
+	accuracy = correct / Total
+	
+	return accuracy
+	    
+	    
+	    
+def accuracy_score(y_true, y_pred):
+    correct = 0
+
+    for true, pred in zip(y_true, y_pred):
+        if true == pred:
+            correct += 1
+
+    return correct / len(y_true)	    
+	    
+	    
+	    
+y_true = np.array([1, 0, 1, 1, 0, 1])
+y_pred = np.array([1, 0, 0, 1, 0, 1])
+output = accuracy_score(y_true, y_pred)
+print(output)
